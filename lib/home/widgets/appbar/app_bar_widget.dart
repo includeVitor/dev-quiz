743f9a185822c67d4431c45f320807329f9d5e39
Vitor Hugo Nunes Da Silva <vitorzcomputacao@gmail.com>
2021-04-21 00:51:58 -0300

    adding score_card and align everything to end

--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:nlw5_flutter/core/app_gradients.dart';
+import 'package:nlw5_flutter/core/app_text_styles.dart';
+import 'package:nlw5_flutter/home/widgets/score_card/score_card_widget.dart';
 
 class AppBarWidget extends PreferredSize {
   AppBarWidget()
@@ -8,21 +10,40 @@ class AppBarWidget extends PreferredSize {
             child: Container(
               height: 250,
               decoration: BoxDecoration(gradient: AppGradients.linear),
-              child: Row(
-                children: [
-                  Text("Olá mundo"),
-                  Container(
-                    width: 58,
-                    height: 58,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      image: DecorationImage(
-                        image: NetworkImage(
-                            "https://avatars.githubusercontent.com/u/38621315?v=4"),
-                      ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text.rich(
+                          TextSpan(
+                              text: "Olá, ",
+                              style: AppTextStyles.title,
+                              children: [
+                                TextSpan(
+                                    text: "Vitor Hugo",
+                                    style: AppTextStyles.titleBold)
+                              ]),
+                        ),
+                        Container(
+                          width: 58,
+                          height: 58,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            image: DecorationImage(
+                              image: NetworkImage(
+                                  "https://avatars.githubusercontent.com/u/38621315?v=4"),
+                            ),
+                          ),
+                        )
+                      ],
                     ),
-                  )
-                ],
+                    ScoreCardWidget()
+                  ],
+                ),
               ),
             ));
 }
