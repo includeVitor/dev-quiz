46f77a3929aa6f19d92a95e9f54716c8235f393e
Vitor Hugo Nunes Da Silva <vitorzcomputacao@gmail.com>
2021-04-21 13:31:38 -0300

    pass a parameter to LevelButtonWidget

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -12,6 +12,10 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBarWidget(), body: LevelButtonWidget());
+    return Scaffold(
+        appBar: AppBarWidget(),
+        body: LevelButtonWidget(
+          label: "Fácil",
+        ));
   }
 }
--- lib/home/widgets/level_button/level_button_widget.dart
+++ lib/home/widgets/level_button/level_button_widget.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:nlw5_flutter/core/app_colors.dart';
 
 class LevelButtonWidget extends StatelessWidget {
-  LevelButtonWidget({Key? key}) : super(key: key);
+  final String label;
+  const LevelButtonWidget({Key? key, required this.label}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +14,14 @@ class LevelButtonWidget extends StatelessWidget {
           border: Border.fromBorderSide(
               BorderSide(color: AppColors.levelButtonBorderFacil)),
           borderRadius: BorderRadius.circular(28)),
-      child: Text("Fácil"),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 26, vertical: 6),
+        child: Text(
+          label,
+          style: GoogleFonts.notoSans(
+              color: AppColors.levelButtonTextFacil, fontSize: 13),
+        ),
+      ),
     );
   }
 }
