546bb13d70bb89b6c283b8afae5bb10ae2c526a9
Vitor Hugo Nunes Da Silva <vitorzcomputacao@gmail.com>
2021-04-25 23:10:39 -0300

    adding animation

--- lib/home/widgets/chart/chart_widget.dart
+++ lib/home/widgets/chart/chart_widget.dart
@@ -2,8 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:nlw5_flutter/core/app_colors.dart';
 import 'package:nlw5_flutter/core/app_text_styles.dart';
 
-class ChartWidget extends StatelessWidget {
-  const ChartWidget({Key? key}) : super(key: key);
+class ChartWidget extends StatefulWidget {
+  final double percent;
+  const ChartWidget({Key? key, required this.percent}) : super(key: key);
+
+  @override
+  _ChartWidgetState createState() => _ChartWidgetState();
+}
+
+class _ChartWidgetState extends State<ChartWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  void initAnimation() {
+    _controller =
+        AnimationController(vsync: this, duration: Duration(seconds: 3));
+    _animation =
+        Tween<double>(begin: 0.0, end: widget.percent).animate(_controller);
+  }
 
   @override
   Widget build(BuildContext context) {
