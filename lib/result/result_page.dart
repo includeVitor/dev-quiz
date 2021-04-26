d1d239ad667c93986d46c7c793fb2b88a10e587a
Vitor Hugo Nunes Da Silva <vitorzcomputacao@gmail.com>
2021-04-25 23:06:23 -0300

    finishing up the quiz

--- lib/result/result_page.dart
+++ lib/result/result_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:nlw5_flutter/challenge/widgets/next_button/next_button_widget.dart';
 import 'package:nlw5_flutter/core/app_images.dart';
 import 'package:nlw5_flutter/core/app_text_styles.dart';
+import 'package:share_plus/share_plus.dart';
 
 class ResultPage extends StatelessWidget {
   final String title;
@@ -59,7 +60,11 @@ class ResultPage extends StatelessWidget {
                         child: Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 68),
                       child: NextButtonWidget.purple(
-                          label: "Compartilhar", onTap: () {}),
+                          label: "Compartilhar",
+                          onTap: () {
+                            Share.share(
+                                'DevQuiz NLW 5 - Flutter: Resultado do Quiz: $title\nObtive ${result / length}% aproveitamento!');
+                          }),
                     )),
                   ],
                 ),
